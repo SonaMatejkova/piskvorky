6d5afff6bb599fa1d571b076bdf1b17352352182
Soňa Matějková <mat.sona53@gmail.com>
2020-10-31 23:30:34 +0100

    Přidána diagonála

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ const isWinningMove = (field) => {
   const symbol = getSymbol(field)
 
   let i
+  let k
   let inRow = 1
   i = position.column
   while (i > 0 && symbol === getSymbol(getField(position.row, i - 1))) {
@@ -95,6 +96,42 @@ const isWinningMove = (field) => {
     i++
   }
   if (inColumn >= winSymbols) {
+    return true
+  }
+  let inRightDiag = 1
+  i = position.row
+  k = position.column
+  while (i > 0 && k > 0 && symbol === getSymbol(getField(i - 1, k - 1))) {
+    inRightDiag++
+    i--
+    k--
+  }
+  i = position.row
+  k = position.column
+  while (i > gridSize -1 && k > gridSize -1 && symbol === getSymbol(getField(i + 1, k + 1))) {
+    inRightDiag++
+    i++
+    k++
+  }
+  if (inRightDiag >= winSymbols) {
+    return true
+  }
+  let inLeftDiag = 1
+  i = position.row
+  k = position.column
+  while (i > 0 && k < gridSize - 1 && symbol === getSymbol(getField(i - 1, k + 1))) {
+    inLeftDiag++
+    i--
+    k++
+  }
+  i = position.row
+  k = position.column
+  while (i < gridSize -1 && k > 0 && symbol === getSymbol(getField(i + 1, k - 1))) {
+    inLeftDiag++
+    i++
+    k--
+  }
+  if (inLeftDiag >= winSymbols) {
     return true
   }
   return false
