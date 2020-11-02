04e79121455270b7ee94b67fe4ca3f28f76c8d42
Soňa Matějková <mat.sona53@gmail.com>
2020-11-02 13:33:44 +0100

    Oprava funkce is WinnigMove

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const vyber = (event) => {
     isWinningMove(event.target);
     if (isWinningMove(event.target)) {
       setTimeout(() => {
-      window.confirm(`Vyhrává hráč s kolečkem! Spustit novou hru?`);
-      location.reload();
+        window.confirm(`Vyhrává hráč s kolečkem! Spustit novou hru?`);
+        location.reload();
       }, 100);
     }
     naTahu = 'krizek';
@@ -26,8 +26,8 @@ const vyber = (event) => {
     isWinningMove(event.target);
     if (isWinningMove(event.target)) {
       setTimeout(() => {
-      window.confirm(`Vyhrává hráč s křížkem! Spustit novou hru?`);
-      location.reload();
+        window.confirm(`Vyhrává hráč s křížkem! Spustit novou hru?`);
+        location.reload();
       }, 100);
     }
     naTahu = 'kolecko';
@@ -35,9 +35,9 @@ const vyber = (event) => {
 };
 
 const buttons = document.querySelectorAll('.field');
-for (let i = 0; i < buttons.length; i ++) {
+for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', vyber);
-};
+}
 
 const gridSize = 10;
 
@@ -45,18 +45,18 @@ const getPosition = (field) => {
   let fieldIndex = 0;
   while (fieldIndex < buttons.length) {
     if (field === buttons[fieldIndex]) {
-      break
+      break;
     }
-    fieldIndex++
+    fieldIndex++;
   }
   return {
-          row: Math.floor(fieldIndex / gridSize),
-          column: fieldIndex % gridSize,
-  }
+    row: Math.floor(fieldIndex / gridSize),
+    column: fieldIndex % gridSize,
+  };
 };
 
 const getField = (row, column) => {
-  return buttons[row * gridSize + column]
+  return buttons[row * gridSize + column];
 };
 
 const getSymbol = (field) => {
@@ -65,78 +65,96 @@ const getSymbol = (field) => {
   } else if (field.className === 'krizek') {
     return 'krizek';
   }
-}; 
+};
 
 const winSymbols = 5;
 const isWinningMove = (field) => {
-  const position = getPosition(field)
-  const symbol = getSymbol(field)
+  const position = getPosition(field);
+  const symbol = getSymbol(field);
 
-  let i
-  let k
-  let inRow = 1
-  i = position.column
+  let i;
+  let k;
+  let inRow = 1;
+  i = position.column;
   while (i > 0 && symbol === getSymbol(getField(position.row, i - 1))) {
-    inRow++
-    i--
+    inRow++;
+    i--;
   }
-  i = position.column
-  while (i < gridSize - 1 && symbol === getSymbol(getField(position.row, i + 1))) {
-    inRow++
-    i++
+  i = position.column;
+  while (
+    i < gridSize - 1 &&
+    symbol === getSymbol(getField(position.row, i + 1))
+  ) {
+    inRow++;
+    i++;
   }
   if (inRow >= winSymbols) {
-    return true
+    return true;
   }
-  let inColumn = 1
-  i = position.row
+  let inColumn = 1;
+  i = position.row;
   while (i > 0 && symbol === getSymbol(getField(i - 1, position.column))) {
-    inColumn++
-    i--
+    inColumn++;
+    i--;
   }
-  i = position.row
-  while (i < gridSize - 1 && symbol === getSymbol(getField(i + 1, position.column))) {
-    inColumn++
-    i++
+  i = position.row;
+  while (
+    i < gridSize - 1 &&
+    symbol === getSymbol(getField(i + 1, position.column))
+  ) {
+    inColumn++;
+    i++;
   }
   if (inColumn >= winSymbols) {
-    return true
+    return true;
   }
-  let inRightDiag = 1
-  i = position.row
-  k = position.column
+  let inRightDiag = 1;
+  i = position.row;
+  k = position.column;
   while (i > 0 && k > 0 && symbol === getSymbol(getField(i - 1, k - 1))) {
-    inRightDiag++
-    i--
-    k--
+    inRightDiag++;
+    i--;
+    k--;
   }
-  i = position.row
-  k = position.column
-  while (i > gridSize -1 && k > gridSize -1 && symbol === getSymbol(getField(i + 1, k + 1))) {
-    inRightDiag++
-    i++
-    k++
+  i = position.row;
+  k = position.column;
+  while (
+    i < gridSize - 1 &&
+    k < gridSize - 1 &&
+    symbol === getSymbol(getField(i + 1, k + 1))
+  ) {
+    inRightDiag++;
+    i++;
+    k++;
   }
   if (inRightDiag >= winSymbols) {
-    return true
+    return true;
   }
-  let inLeftDiag = 1
-  i = position.row
-  k = position.column
-  while (i > 0 && k < gridSize - 1 && symbol === getSymbol(getField(i - 1, k + 1))) {
-    inLeftDiag++
-    i--
-    k++
+  let inLeftDiag = 1;
+  i = position.row;
+  k = position.column;
+  while (
+    i > 0 &&
+    k < gridSize - 1 &&
+    symbol === getSymbol(getField(i - 1, k + 1))
+  ) {
+    inLeftDiag++;
+    i--;
+    k++;
   }
-  i = position.row
-  k = position.column
-  while (i < gridSize -1 && k > 0 && symbol === getSymbol(getField(i + 1, k - 1))) {
-    inLeftDiag++
-    i++
-    k--
+  i = position.row;
+  k = position.column;
+  while (
+    i < gridSize - 1 &&
+    k > 0 &&
+    symbol === getSymbol(getField(i + 1, k - 1))
+  ) {
+    inLeftDiag++;
+    i++;
+    k--;
   }
   if (inLeftDiag >= winSymbols) {
-    return true
+    return true;
   }
-  return false
+  return false;
 };
